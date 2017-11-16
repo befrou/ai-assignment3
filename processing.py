d98b3f1a8313b7938c32c0765b9a574f33f4b4e7
Bruno Klein <bruno.klein@acad.pucrs.br>
2017-11-15 23:02:56 -0200

    Tokenize / Normalize / Grammar info

--- processing.py
+++ processing.py
@@ -6,8 +6,11 @@ import re
 # http://python-notes.curiousefficiency.org/en/latest/python3/text_file_processing.html
 # https://blogs.msdn.microsoft.com/oldnewthing/20140930-00/?p=43953
 # https://pypi.python.org/pypi/chardet
+# https://stackoverflow.com/questions/9525993/get-consecutive-capitalized-words-using-regex
 
 cogroo = Cogroo.Instance()
+lemmatized_words = []
+grammar_info = {}
 
 def clean_text(text):
     pattern1 = re.compile(r'[\x93\x94\x96\x97]')
@@ -20,42 +23,68 @@ def clean_text(text):
 
     return clean_text
 
-def lemmatize_file(s_path, d_path):
-    # Arquivo 'problema' usar utf-16le. Nos demais utilizar latin-1
-    with open(s_path, encoding='latin-1') as a_file1:
-        with open(d_path, 'w', encoding='latin-1') as a_file2:
-            for a_line in a_file1:
-                clean_line = clean_text(a_line)
-                lemma_line = cogroo.lemmatize(clean_line)
-                a_file2.write(lemma_line + '\n')
+# Tokenize and Normalize
+def get_list_of_words(line):
+    pattern1 = re.compile("([A-Z][a-zÀ-ÿ]+(?=\s[A-Z])(?:\s[A-Z][a-zÀ-ÿ]+)+)")
+    pattern2 = re.compile("\w+")
+
+    words = []
+
+    # Unify with underline sequence of words that start with Capital Letter
+    if pattern1.match(line):
+        aux = re.findall(pattern1, line)
+        
+        for a in aux:
+            line = line.replace(a, '')
+            b = re.sub(r'\s', '_', a)
+            words.append(b)
+
+    # Get the rest of the words
+    aux = re.findall(pattern2, line)
+    
+    for a in aux:
+        words.append(a)
+
+    return words
+    
+def process_text(s_path):
+    with open(s_path, encoding='latin-1') as a_file1:  
+        for a_line in a_file1:
+            clean_line = clean_text(a_line)
+            words = get_list_of_words(a_line)
+            
+            for word in words:
+                aux = cogroo.lemmatize(word)
+                avaliated_word = cogroo.analyze(aux)
+
+                if avaliated_word.sentences:
+                    grammar_class = re.findall(r'\#\w+\-*', str(avaliated_word.sentences[0].tokens))
+
+                grammar_info[aux] = grammar_class[0]
+                lemmatized_words.append(aux)
+                
+
 
 # PRÉ-PROCESSAMENTO: aplica lematização em todos arquivos no folder 'pre-processing'
 # obs: a codificação dos caracteres pode variar para cada arquivo o que pode acarretar em erros
-def lemmatize_files():
-    directory_in = "pre-processing"
-    directory_out = "post-processing/output"
-    n_file = 0
+# def lemmatize_files():
+#     directory_in = "pre-processing"
+#     directory_out = "post-processing/output"
+#     n_file = 0
 
-    for filename in os.listdir(directory_in):
-        full_path1 = os.path.join(directory_in, filename)
-        full_path2 = directory_out + str(n_file) + '.txt'
+#     for filename in os.listdir(directory_in):
+#         full_path1 = os.path.join(directory_in, filename)
+#         full_path2 = directory_out + str(n_file) + '.txt'
 
-        with open(full_path1, encoding='latin-1') as a_file1:
-            with open(full_path2, 'w', encoding='latin-1') as a_file2:
-                for a_line in a_file1:
-                    clean_line = clean_text(a_line)
-                    lemma_line = cogroo.lemmatize(clean_line)
+#         with open(full_path1, encoding='latin-1') as a_file1:
+#             with open(full_path2, 'w', encoding='latin-1') as a_file2:
+#                 for a_line in a_file1:
+#                     clean_line = clean_text(a_line)
+#                     lemma_line = cogroo.lemmatize(clean_line)
 
-                    a_file2.write(lemma_line)
-        n_file += 1
-
-# TESTANDO
-def extract_info():
-    with open("post-processing/esporte-teste.txt", encoding='latin-1') as a_file1:
-        for a_line in a_file1:
-            a = cogroo.analyze(a_line)
-            print(a.sentences[0].tokens[0])
-            break
+#                     a_file2.write(lemma_line)
+#         n_file += 1
 
 if __name__ == "__main__":
-   lemmatize_file("pre-processing/CORPUS DG POLICIA - final.txt", "post-processing/teste.txt")
+    process_text('pre-processing/CORPUS DG POLICIA - final.txt')
+    print(grammar_info)
